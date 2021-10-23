d6e65455395ccaa04e85f281298f8e31f72055c9
Dayo1900 <73638760+Dayo1900@users.noreply.github.com>
2021-10-23 16:28:54 +0100

    Create server_searchEmail.php

---
+++ server_searchEmail.php
@@ -0,0 +1,32 @@
+<?php
+error_reporting(0);
+//connect to database
+$conn = mysqli_connect('localhost', 'id17048003_gahs', 'Temitope.1900', 'id17048003_customer');
+
+
+//check connection
+if (!$conn) 
+{ 
+  die("Connection failed: Unable to connect to server");
+}
+
+
+$searchEmail = $_SESSION["email_reset_passwd"];
+
+
+//check if email is registered.
+$check = mysqli_query($conn, "SELECT email FROM registration WHERE email='$searchEmail'");
+$row = mysqli_num_rows($check);
+
+if($row > 0) 
+{}
+
+else 
+{
+  die("<p class='design' style='text-align:center; color:purple;'>" .$searchEmail. " is not registered on this site.</p>");
+} 
+
+
+mysqli_close($conn);
+
+?>
