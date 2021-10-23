554b77f79fc0b0fd6ff83f90ee89f4db428f8db2
Dayo1900 <73638760+Dayo1900@users.noreply.github.com>
2021-10-23 16:17:24 +0100

    Update itex_withdrawMoney.php

--- itex_withdrawMoney.php
+++ itex_withdrawMoney.php
@@ -20,19 +20,86 @@ return false;
 <meta name="keywords" content="'itex', 'wallet'">
 
 <link rel="stylesheet" type="text/css" href="itex_design.css">
-<link rel="stylesheet" type="text/css" media="(max-width:990px)" href="itex_design990px.css">
-<link rel="stylesheet" type="text/css" media="(max-width:630px)" href="itex_design630px.css">	
+<link rel="stylesheet" type="text/css" media="(max-width:1199px)" href="itex_design1199px.css">
+<link rel="stylesheet" type="text/css" media="(max-width:860px)" href="itex_design860px.css">
+<link rel="stylesheet" type="text/css" media="(max-width:600px)" href="itex_design600px.css">
 <link rel="stylesheet" type="text/css" media="(max-width:330px)" href="itex_design330px.css">
 </head>
 
 <body style="background-color:#f9f2ec;">
  
     <!--steps to take after user clicks the transfer button-->
-<?php if (isset ($_POST["transfer"]))
+<!--Design the dashboard page--> 
+  <h1 style="text-align:center; color:#003399; margin-top:2.5%; font-size:2.5rem;"> Withdraw </h1>
+  <p class="design" style="margin-top:none;"> Transfer money from itex wallet to bank account.</p>
+
+
+<form method="post" action="itex_withdrawMoney.php">
+<div class="a2" style="padding-top:2%;">
+<h6 class="design" style="color:purple;"> Note: This is only a demo, do not submit real bank details here!</h6>
+
+<div class="align">
+<label for="withdrawMoney" class="d2">Amount to transfer: </label>
+   <input class="card" type="number" name="withdrawMoney" placeholder= "0.00"> 
+   <span class="error"> 
+      <small><?php if (isset ($_POST["transfer"]) && (!is_numeric($_POST["withdrawMoney"])))   //This will ensure that a number is submitted
+                             {echo "<br><p class='design'>Incorrect Details</p>";
+                             return false;
+                                exit();} ?> 
+   </small>
+   </span>
+</div>
+
+<div class="align">
+  <label for="account_number" class="d2">Account number<br>10-digits: </label>
+   <input class="card" type="text" name="account_number" placeholder="0000000000"> 
+   <span class="error">
+   <small><?php if (isset ($_POST["transfer"]) && ((mb_strlen($_POST["account_number"]) !==10)))       //This will ensure that account number is submitted 
+                             {echo"<p><br><br>Enter account number.</p>";
+                             return false;
+                             exit();} ?>     
+   </small>
+   </span>
+</div>
+
+<div class="align"> 
+<label for="account_name" class="d2">Account name:</label>
+   <input class="card" style="margin-right:none;" type="text" name="account_name"> 
+   <span class="error">
+   <small><?php if (isset ($_POST["transfer"]) && (empty($_POST["account_name"])))   //This will ensure that account name is submitted
+                             {echo "<p><br> Enter account name.</p>";  return false;
+exit();} ?> 
+   </small>
+   </span>
+</div>
+
+
+<div class="align">
+   <label for="bank" class="d2">Bank name: </label>
+   <input class="card" type="text" name="bank"> 
+   <span class="error"> 
+      <small><?php if (isset ($_POST["transfer"]) && (empty($_POST["bank"])))   //This will ensure that bank is submitted
+                             {echo("<p><br> Enter name of bank.</p>");   return false;
+exit();} ?> 
+   </small>
+   </span>
+</div>
+
+<input class="c" style="margin-top:10%;" type="submit" name="transfer" value="Transfer money">
+
+<input type="hidden" name="id" value="<?php echo $_SESSION['id'];?>">    <!--This will submit the user ID-->
+
+</form>
+</div>
+       
+       
+       <?php if (isset ($_POST["transfer"]))
 {
+error_reporting(0);
+
 
 //define variables and set to empty values
-$account_number=$account_name=$bank=$withdrawMoney=$id=""; 
+$card_number=$card_validity=$card_cvv=$addMoney=$id=""; 
 
 //create a function to sanitise the data input from the client-side
  function check_input($data)
@@ -45,65 +112,24 @@ $account_number=$account_name=$bank=$withdrawMoney=$id="";
  
  
 // sanitise input from client side once submitted
-$account_number=check_input($_POST["account_number"]);
 $account_name=check_input($_POST["account_name"]);
 $bank=check_input($_POST["bank"]);
-$withdrawMoney=check_input($_POST["withdrawMoney"]);
+$account_number=check_input($_POST["account_number"]);
 $id=$_POST["id"];
 
 
-include 'server_withdrawMoney.php';          //this will include script which will submit transaction to database.
+if(is_numeric($_POST["withdrawMoney"]))
+{ 
+    $withdrawMoney = $_POST["withdrawMoney"];
+    
+    include 'server_withdrawMoney.php';          //this will include script which will submit transaction to database.
+}
 
-//In a real situation, $account_number, $account_name, $bank and $withdrawMoney would be forwarded to the appropriate Bank API for processing.
+//In a real situation, all the variables above would be forwarded to the specified bank for verification. But I don't have their API so I can't take this step.
 }
 ?>
-
-
-<!--Design the dashboard page--> 
-  <h1 style="text-align:center; color:#003399; margin-top:2.5%; font-size:2.5rem;"> Withdraw </h1>
-  <p class="design" style="margin-top:none;"> Transfer money from itex wallet to bank account.</p>
-
-
-<form method="post" action="itex_withdrawMoney.php">
-<div class="a2" style="padding-top:2%;">
-<h6 class="design" style="color:red;"> Note: This is only a demo, do not submit real bank details here!</h6>
-
-<div class="align">
-<label for="withdrawMoney" class="d2">Amount to transfer: </label>
-   <input class="card" type="number" name="withdrawMoney" placeholder= "0.00"> 
-</div>
-
-<div class="align">
-  <label for="account_number" class="d2">Account number: </label>
-   <input class="card" type="number" name="account_number" placeholder="0000000000"> 
-   <span class="error"> <?php if (isset ($_POST["transfer"]) && (empty($_POST["account_number"])))
-                             {echo "<p> Enter account number.</p>";}      //This will ensure that account number is submitted
-                             ?> </span>
-</div>
-
-<div class="align"> 
-<label for="account_name" class="d2">Account name:</label>
-   <input class="card" style="margin-right:none;" type="text" name="account_name"> 
-   <span class="error"> <?php if (isset ($_POST["transfer"]) && (empty($_POST["account_name"])))   //This will ensure that account name is submitted
-                             {echo "<p> Enter account name.</p>";} ?> 
-                             </span>
-</div>
-
-
-<div class="align">
-   <label for="bank" class="d2">Bank name: </label>
-   <input class="card" type="text" name="bank"> 
-   <span class="error"> <?php if (isset ($_POST["transfer"]) && (empty($_POST["bank"])))   //This will ensure that bank is submitted
-                             {echo "<p> Enter name of bank.</p>";} 
-                             ?> </span>
-</div>
-
-<input class="c" style="margin-top:10%;" type="submit" name="transfer" value="Transfer money">
-
-<input type="hidden" name="id" value="<?php echo $_SESSION['id'];?>">    <!--This will submit the user ID-->
-
-</form>
-</div>
+       
+       
        <br><br><br>     
 </body>
 </html>
