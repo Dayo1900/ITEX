f4addaa15f63b9bbd4790e339a4ce4c3a338b43b
Dayo1900 <73638760+Dayo1900@users.noreply.github.com>
2021-10-23 16:26:25 +0100

    Update server_itexRegister.php

--- server_itexRegister.php
+++ server_itexRegister.php
@@ -1,45 +1,65 @@
 <?php
-
-
-// initiate connection with database
-
-
+error_reporting(0);
 //connect to database
-$conn = new mysqli('localhost', 'id17048003_gahs', 'Temitope.1900', 'id17048003_customer');
+$conn = mysqli_connect('localhost', 'id17048003_gahs', 'Temitope.1900', 'id17048003_customer');
+
 
 //check connection
-if ($conn->connect_error) 
+if (!$conn) 
 { 
-  die("Connection failed: " . $conn->connect_error);
+  die("Connection failed: Unable to connect to server");
 }
 
+
+
+$fname = $_SESSION['fname'];
+$sname = $_SESSION['sname'];
+$email = $_SESSION['email_register'];
+$password_1 = $_SESSION['password_1'];
+$password_2 = $_SESSION['password_2'];
+$nin = $_SESSION['nin'];
+
+
+
+//Table registration is a table created to keep record of registered emails so as to avoid email duplication
 //check if email has already been registered.
-$check = "SELECT email FROM registration WHERE email='$email'";
-$response = $conn->query($check);
+$check = mysqli_query($conn, "SELECT email FROM registration WHERE email='$email'");
+$row = mysqli_num_rows($check);
 
-if($response->num_rows >0) 
+if($row > 0) 
 {
- printf("<p class='error'>Email already registered, try another.<br></p>");
+ printf("<p class='design' style='text-align:center; color:purple;'>Email already registered, try another.<br></p>");
  return false;
+ exit();
 }
+elseif ($password_1 !== $password_2)
+{ 
+    echo "<p class='design' style='text-align:center; color:purple;'>Password not matching. </p>";
+    return false;
+    exit();
+}
+else 
+{
+    $password = md5($password_1);       //encrypt the password before saving in the database
+} 
 
-if (($_POST["password_1"]) !== ($_POST["password_2"]))
-{return false;}
-else {$password = md5($password_1);} //encrypt the password before saving in the database
+
 
 //insert data into database
-$submit_1 = "INSERT INTO registration (fname, sname, email, nin, password) 
-VALUES ('$fname', '$sname', '$email', '$nin', '$password')";
-
+$submit_1 = "INSERT INTO registration (fname, sname, email, nin, password) VALUES ('$fname', '$sname', '$email', '$nin', '$password')";
 
 if( mysqli_query($conn, $submit_1))
 {
-printf("<p class='design' style='color:purple;'> Wallet created, you may log in.</p>");
+printf("<p class='design' style='text-align:center; color:purple;'> Registration complete.</p>");
+echo "<meta http-equiv='refresh'  content='2;url=itex_login.php'>";
 }
 else
 {
-echo "<p class='design'>Unable to register at the moment: </p>" . mysqli_error($conn);
+echo "<p class='design' style='text-align:center; color:purple;'>Unable to register at the moment: </p>";
 }
-$conn->close();
+
+
+
+mysqli_close($conn);
 
 ?>
