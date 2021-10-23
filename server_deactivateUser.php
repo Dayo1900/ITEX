9b3b5a23fecde06da86930946ee470e9c1ab41df
Dayo1900 <73638760+Dayo1900@users.noreply.github.com>
2021-10-23 16:18:55 +0100

    Update server_deactivateUser.php

--- server_deactivateUser.php
+++ server_deactivateUser.php
@@ -1,54 +1,122 @@
 <?php
 
-
 // initiate connection with database
-
-
-//connect to database
 $conn = mysqli_connect('localhost', 'id17048003_gahs', 'Temitope.1900', 'id17048003_customer');
+
 
 //check connection
 if (!$conn) 
-{ 
-  die("Connection failed: " . mysqli_connect_error());
+{
+  die("Connection failed: Unable to connect to server");
 }
-
 
 //There is a separate page named 'create database' where I showed how I created two tables: 'registration' and 'store_account'.
-//1. table registration to hold information of normal users
-//2. table store_account to hold information of deactivated users
-//3. this page shows how to remove deactivated users from table registration and insert into table store_account
 
-$_SESSION['id'] = $id;
-$_SESSION['fname'] = $fname;
-$_SESSION['sname'] = $sname;
-$_SESSION['email'] = $email;
-$_SESSION['password'] = $password;
-$_SESSION['nin'] = $nin;
-$_SESSION['account_balance'] = $account_balance;
+//1. table registration to hold information of normal users
+
+//2. table store_account to hold information of deactivated users
+
+//3. this page shows how to remove users from table registration and insert into table store_account during deactivation
+
+$id = $_SESSION['id'];
+
+$fname = $_SESSION['fname'];
+
+$sname = $_SESSION['sname'];
+
+$email = $_SESSION['email'];
+
+$nin = $_SESSION['nin'];
+
+$account_balance = $_SESSION['account_balance'];
+
+
+
+//if there's still some money in account_balance, don't deactivate wallet
+if($account_balance > 5)
+{
+ $cash = number_format($account_balance);
+ echo "<p class='design' style='font-size:1.2rem; text-align:center;'>You still have some money in your account, empty it to deactivate wallet.</p>";
+  return false;
+  exit();
+}
+elseif($account_balance == NULL)
+{
+    $account_balance = 0;
+}
+elseif($nin == NULL)
+{
+    $nin = 0;
+}
+else {}
+
+
 
 //insert user basic details into store_account 
+
 //because the user is still logged in, I can make use of session variables
-$deactivate="INSERT INTO store_account VALUES ($id, '$fname', '$sname', '$email', '$password', '$nin', '$account_balance' )";
+$deactivate = "INSERT INTO store_account (fname, sname, email, nin, account_balance) VALUES ('$fname', '$sname', '$email', '$nin', $account_balance);";
 
 //after inserting
-if (mysqli_query($conn, $deactivate))
+if(mysqli_query($conn, $deactivate))
 {
-    //delete user basic info from table registration
-$delete = "DELETE FROM registration WHERE ID = $id";
-
-    //unset and destroy user session
-    session_unset();
-    
-    session_destroy();
-    
-    //redirect back to home page
-    echo "<meta http-equiv='refresh'  content='2;url=itex_homepage.php'>";
-    
+echo "<p class='design' style='font-size:1.2rem; text-align:center; color:purple;'>Processing</p>";    
+}
+else {
+    echo "Unable to connect to database";
+    exit();
 }
 
-else { echo "Unable to deactivate account at the moment" . mysqli_error();}
+//delete user basic info from table registration;
+$delete = "DELETE FROM registration WHERE ID = $id";
+
+//unset and destroy user session
+session_unset();
+    
+session_destroy();
+
+
+
+
+    
+if(mysqli_query($conn, $delete))
+{
+//redirect back to home page
+echo "<meta http-equiv='refresh'  content='2;url=itex_homepage.php'>";
+
+echo "<p class='design' style='font-size:1.2rem; text-align:center; color:purple;'>Wallet deactivated successfully.</p>";
+}
+else {
+ echo "<p class='design' style='font-size:1.2rem; text-align:center;'>Unable to deactivate wallet at the moment</p>";
+ exit();
+}
+
 
 mysqli_close($conn);
 
+
 ?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
