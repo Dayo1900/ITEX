7e01693911aada01abe6bfc716a09594857dd8a7
Dayo1900 <73638760+Dayo1900@users.noreply.github.com>
2021-11-16 02:04:27 +0100

    Update itex_homepage.php

--- itex_homepage.php
+++ itex_homepage.php
@@ -42,7 +42,24 @@
 
 <br><br>
 <a class="login2" href="itex_reactivateUser.php"> Reactivate wallet</a>
+<br><br>
 
+<div class="explanation">
+<p class="explanation ">
+    ITEX online wallet is a demo website and it can:<br>
+
+    --- register new users - and create a new wallet for them<br>
+    --- they can login or reset their password <br>
+    --- users can add money to their wallet <br>
+    --- they can withdraw and also view transaction history <br>
+    --- they can deactivate their wallet <br>
+    --- they can also reactivate their wallet <br>
+
+I included email notifications using PHPmailer.
+</p>
+</div>
+
+<footer> <p style="text-align:center;"> Created by Dayo Abdul ©2021</p></footer>
 </body>
 
 </html>
