50d8c5617085a7b0ea019e14f1add5d68b3c77ee
Dayo1900 <73638760+Dayo1900@users.noreply.github.com>
2022-03-24 12:19:44 +0100

    Update

--- server_searchEmail.php
+++ server_searchEmail.php
@@ -1,7 +1,7 @@
 <?php
 error_reporting(0);
 //connect to database
-$conn = mysqli_connect('localhost', 'id17048003_gahs', 'Temitope.1900', 'id17048003_customer');
+$conn = mysqli_connect('****', '*****', '*****', '*****');
 
 
 //check connection
